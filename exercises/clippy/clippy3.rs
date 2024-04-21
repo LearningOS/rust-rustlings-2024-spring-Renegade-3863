764b0aa97aab7e6e081cc0e86d7a6f6d536b73bf
Code_Hotaru <115505668+Renegade-3863@users.noreply.github.com>
2024-04-21 14:24:01 +0000

    clippy3.rs resubmission

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -4,8 +4,6 @@
 //
 // Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
 
-use std::mem;
-
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
@@ -26,6 +24,6 @@ fn main() {
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    mem::swap(&mut value_a, &mut value_b);
+    std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
